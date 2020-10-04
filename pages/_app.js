8820754d5c237bd37c80298e5761344d43634485
ZaraKinneen <zarakinneen@gmail.com>
2020-10-04 14:47:07 +0100

    added styled compants and favicon and removed unused code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle` 
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  *{
+    box-sizing: border-box;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#355C7D",
+    secondary: "#C06C84",
+  },
+  fonts: {
+    light: "#FFFFFF",
+    dark: "#000000",
+    highlight: "#FFCC00",
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
