f0d13be3ea81f957c9dbb45652123df083d4f1c6
ZaraKinneen <zarakinneen@gmail.com>
2020-10-04 15:37:56 +0100

    added custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,58 @@
+import HeaderComponent from "../components/Header";
+import styled from "styled-components";
+import Link from "next/link";
+
+const Container = styled.div`
+  height: 80vh;
+  text-align: center;
+  align-items: center;
+  background: ${({ theme }) => theme.colors.primary};
+`;
+
+const Image = styled.img`
+  width: 100%;
+  max-width: 20em;
+  height: auto;
+  margin-top: 10em;
+`;
+
+const Text = styled.p`
+  // font-size: 1.8em;
+  color: ${({ theme }) => theme.fonts.light};
+  font-weight: bold;
+`;
+
+const LinkContainer = styled.span`
+  ${Text} {
+    padding: 1em;
+    margin: 0;
+  }
+  &:hover ${Text} {
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.secondary};
+  }
+  &:hover ${Image} {
+    transform: rotate(20deg);
+  }
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding-top: 2em;
+  color: ${({ theme }) => theme.fonts.highlight};
+`;
+
+export default () => (
+  <>
+    <HeaderComponent title="Random Beer App" />
+    <Container>
+      <Title>Looks like you are lost...</Title>
+      <LinkContainer>
+        <Image src="./beer.png" alt="beer" />
+        <Link href={"/"}>
+          <Text>Take Me Back to the Beer</Text>
+        </Link>
+      </LinkContainer>
+    </Container>
+  </>
+);
